5415b6f78f8c07c42916d88d60b79e29fb74d613
Yash <yashmahalle33@gmail.com>
2025-12-05 11:48:30 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import RelativeList from './RealativeList'
 
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <RelativeList/>
     </div>
   )
 }
